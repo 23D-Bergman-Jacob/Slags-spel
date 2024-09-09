41344e7a0a15b6b0adf995d5a996bb7c0088b9ca
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2024-09-09 09:57:50 +0200

    first commit

---
+++ Slagsspel/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random rnd = new Random();
+string sk1;
+string sk2;
+int sk1hp = rnd.Next(220, 270);
+int sk2hp = rnd.Next(230, 280);
+int sk1s;
+int sk2s;
+int slag = 0;
+System.Console.WriteLine("Vad heter slagskämpe 1?");
+sk1 = Console.ReadLine();
+System.Console.WriteLine("Vad heter slagskämpe 2?");
+sk2 = Console.ReadLine();
+Console.Clear();
+while (1==1){
+Console.ReadLine();
+slag ++;
+sk1s = rnd.Next(22, 34);
+sk2s = rnd.Next(20, 33);
+sk1hp = sk1hp - sk2s;
+sk2hp = sk2hp - sk1s;
+System.Console.WriteLine("Efter slag "+ slag + " har "+ sk1 +" "+ sk1hp +" liv kvar och "+sk2+ " "+sk2hp + " kvar");
+if (sk1hp < 1){ break;}
+if (sk2hp < 1){ break;}
+}
+if (sk1hp>0){
+    System.Console.WriteLine("Grattis "+ sk1 + "!!!!");
+}
+if (sk2hp>0){
+    System.Console.WriteLine("Grattis "+ sk2+"!!!!!");
+}
+if (sk2hp<0&sk1hp<0){System.Console.WriteLine("Oavgjort!");}
+Console.ReadLine();
