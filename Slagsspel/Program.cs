61de6500927c15213d06ced2c2d1fc4be5b14f34
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2024-09-09 10:05:09 +0200

    slagspel

--- Slagsspel/Program.cs
+++ Slagsspel/Program.cs
@@ -2,8 +2,8 @@
 Random rnd = new Random();
 string sk1;
 string sk2;
-int sk1hp = rnd.Next(220, 270);
-int sk2hp = rnd.Next(230, 280);
+int sk1hp = rnd.Next(210, 290);
+int sk2hp = rnd.Next(220, 280);
 int sk1s;
 int sk2s;
 int slag = 0;
@@ -11,12 +11,12 @@ System.Console.WriteLine("Vad heter slagskämpe 1?");
 sk1 = Console.ReadLine();
 System.Console.WriteLine("Vad heter slagskämpe 2?");
 sk2 = Console.ReadLine();
-Console.Clear();
+// Console.Clear();
 while (1==1){
 Console.ReadLine();
 slag ++;
-sk1s = rnd.Next(22, 34);
-sk2s = rnd.Next(20, 33);
+sk1s = rnd.Next(20, 30);
+sk2s = rnd.Next(19, 31);
 sk1hp = sk1hp - sk2s;
 sk2hp = sk2hp - sk1s;
 System.Console.WriteLine("Efter slag "+ slag + " har "+ sk1 +" "+ sk1hp +" liv kvar och "+sk2+ " "+sk2hp + " kvar");
