95164837983c3bb3a301cc0ea88bcbf6ea7cb555
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2024-09-13 10:06:38 +0200

    Färdigt

--- Slagsspel/Program.cs
+++ Slagsspel/Program.cs
@@ -2,8 +2,8 @@
 Random rnd = new Random();
 string sk1;
 string sk2;
-int sk1hp = rnd.Next(210, 290);
-int sk2hp = rnd.Next(220, 280);
+int sk1hp = rnd.Next(220, 290);
+int sk2hp = rnd.Next(230, 280);
 int sk1s;
 int sk2s;
 int slag = 0;
@@ -16,7 +16,7 @@ while (1==1){
 Console.ReadLine();
 slag ++;
 sk1s = rnd.Next(20, 30);
-sk2s = rnd.Next(19, 31);
+sk2s = rnd.Next(20, 30);
 sk1hp = sk1hp - sk2s;
 sk2hp = sk2hp - sk1s;
 System.Console.WriteLine("Efter slag "+ slag + " har "+ sk1 +" "+ sk1hp +" liv kvar och "+sk2+ " "+sk2hp + " kvar");
@@ -31,3 +31,8 @@ if (sk2hp>0){
 }
 if (sk2hp<0&sk1hp<0){System.Console.WriteLine("Oavgjort!");}
 Console.ReadLine();
+//correct();
+//static void correct()
+// {
+// upprepande kåd
+// }
